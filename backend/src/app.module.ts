83b415353d30c13d6372badeed41c0f220a325ac
Maciek8862 <maciekkrasnodebski065@gmail.com>
2026-01-04 22:51:01 +0100

    Refactor project structure: remove default app, add modules backend and pages/frontend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,7 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+﻿import { Module } from '@nestjs/common';
+import { MessagesModule } from './modules/messages/messages.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [MessagesModule], // <--- bardzo ważne
 })
-export class AppModule {}
+export class AppModule { }
